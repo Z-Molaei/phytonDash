00adbb5951aa92b733b3ebdac1d193455d26ae04
z-molaei <work3447@gmail.com>
2023-02-20 15:30:48 +0330

    tag

---
+++ 10.py
@@ -0,0 +1,23 @@
+from dash import Dash, html, dcc, Input, Output
+import plotly.express as px
+
+df = px.data.stocks()
+
+app = Dash(__name__)
+app.layout = html.Div(children=[
+    dcc.Dropdown(df.columns[1:], value=df.columns[1], id='input', multi=True),
+    html.Br(),
+    html.Hr(),
+    dcc.Graph(id='output_chart')
+])
+
+
+@app.callback(Output('output_chart', 'figure'),
+              Input('input', 'value'))
+
+def update_chart(x):
+    fig = px.line(df, x='date', y=x)
+    return fig
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
